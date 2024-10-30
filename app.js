a042f05f1fde7f6159c496468c48c74607d21511
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2024-10-30 17:04:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+// let str = "Saylani Mass It Training";
+// console.log(str);
+// // str = str.slice(0 , 7);
+// console.log(str);
+// str = str.slice(8 , 15);
+// console.log(str);
+
+
+// let str = "Saylani Mass It Training";
+// console.log(str);
+// console.log(str.slice(13 , 24));
+
+
+// slice(starting value , ending value)
+// let str = "Saylani Mass It Training";
+// console.log(str);
+// console.log(str.slice(8,15));
+
+// let str = "Saylani Mass It Training";
+// console.log(str);
+// console.log(str.slice(16 , 24));
+
+
+// let str = "Saylani Mass It Training";
+// console.log(str);
+// let copy = str.slice(4 , 7);
+// console.log(copy);
+
+
+//To Lower Case
+// let day = prompt("What day is today");//MONDAY
+// day = day.toLowerCase();//monday
+// if(day == 'monday' || day == 'tuesday' || day == 'wednesday' || day == 'thursday' || day == 'saturday'){
+//   document.write("Working Days");
+// }
+
+// else if(day == 'friday' || day == 'sunday'){
+//     document.write("HoliDays");
+// }
+
+
+//to Upper Case
+// let day = prompt("What day is today");//monday
+// day = day.toUpperCase();//MONDAY
+// if(day == 'MONDAY' || day == 'TUESDAY' || day == 'WEDNESDAY' || day == 'THURSDAY' || day == 'SATURDAY'){
+//   document.write("Working Days");
+// }
+
+// else if(day == 'FRIDAY' || day == 'SUNDAY'){
+//     document.write("HoliDays");
+// }
+
+
+// let month = prompt("Enter a month name");
+// let charInMonth = month.length;
+// if(charInMonth >= 3){
+//     document.write(month.slice(0 , 3));
+// }
+
+//practice
+// let str = prompt("Enter some text");
+// let numChars = str.length;
+
+// for (let i = 0; i < numChars; i++) {
+//     if (str.slice(i, i + 2)) { // str = maham.copy(0 , 0+2) ma
+//         document.write(str);
+//         break;
+//     }
+// }
+
+
+
+// let str = prompt("Enter some text");
+// let numChars = str.length;
+
+// for (let i = 0; i < numChars; i++) {
+//     if (str.slice(i, i + 2) === " ") { // str = maham.copy(0 , 0+2) ma
+//         alert("No double spaces!");
+//         break;
+//     }
+//     else("double spaces!");
+
+// }
+
+
+
+
+
